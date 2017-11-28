9c3a00e7421cad6e5b96151b3ea05128262bd64a
Jonathan Tsang <jtsang@pivotal.io>
2017-11-28 14:13:16 +0000

    new dependency
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>

--- somelib.go
+++ somelib.go
@@ -1,6 +1,14 @@
 package somelib
 
+import (
+	"fmt"
+
+	"github.com/pivotal-cf/on-demand-services-sdk/serviceadapter"
+)
+
 func DoIt() string {
+	a := serviceadapter.Errand{}
+	fmt.Println(a)
 	return "Done it"
 }
 
